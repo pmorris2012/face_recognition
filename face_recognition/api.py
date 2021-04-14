50d7da11a9cf55a68e34734496a0232582fc0b16
Paul Morris <pmorris2012@fau.edu>
2021-04-14 15:43:42 -0400

    joint landmark and embedding api function

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -218,6 +218,24 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 
+def face_encodings_and_landmarks(face_image, known_face_locations=None, num_jitters=1, model="large"):
+    """
+    Given an image, return the 128-dimension face encoding and landmarks for each face in the image.
+
+    :param face_image: The image that contains one or more faces
+    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
+    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :param model: Optional - which model to use. "large" or "small" (default) which only returns 5 points but is faster.
+    :return: A list of 128-dimensional face encodings (one for each face in the image)
+    """
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
+    
+    encodings = [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
+    landmarks = np.array([[(p.x, p.y) for p in landmark.parts()] for landmark in raw_landmarks])
+
+    return encodings, landmarks
+
+
 def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
     """
     Compare a list of face encodings against a candidate encoding to see if they match.
